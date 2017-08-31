2afd10c9871561434a02cc245216ccb8e2506460
Vetula_Mortem <renesebestian90@gmail.com>
2017-08-31 14:22:54 +0200

    sfdghj

--- src/dasfghj/Ichhabesetterundgetter.java
+++ src/dasfghj/Ichhabesetterundgetter.java
@@ -3,4 +3,17 @@ package dasfghj;
 public class Ichhabesetterundgetter {
 private int zahl;
 private boolean wahrheit;
+public int getZahl() {
+	return zahl;
+}
+public void setZahl(int zahl) {
+	this.zahl = zahl;
+}
+public boolean isWahrheit() {
+	return wahrheit;
+}
+public void setWahrheit(boolean wahrheit) {
+	this.wahrheit = wahrheit; //asfdgh
+}
+
 }
